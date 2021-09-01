daef7ed53b2531d3b96f4b613c69cf20ff251984
MascarBone <simone.bonelli@gmail.com>
2021-09-01 18:23:18 +0200

    Correction, genNum: min value

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ console.log(mineField);
 // Funzione per generare un numero casuale compreso in un intervallo (min e max inclusi)
 function genNum (max, min)
 {
-    return Math.floor(Math.random() * (max - min + 1)) +1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Funzione per creare un array, aggiungendo un numero compreso tra 1 e 100 (mai un duplicato)
